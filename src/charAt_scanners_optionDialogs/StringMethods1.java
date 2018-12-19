8c9a2213b872f54cfd2de1b7cd8de4b2333f69c3
Dong7786 <jiwenzou@gmail.com>
2018-12-18 19:57:21 -0800

    changecalcualot

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -6,18 +6,31 @@ package charAt_scanners_optionDialogs;
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-String yeetyeetyeetyeet="yeetyeetyeetblahblahwhywhy";
+String yeetyeetyeetyeet="Who will believe my verse in time to come," + 
+		"If it were fill'd with your most high deserts?" + 
+		"Though yet, heaven knows, it is but as a tomb" + 
+		"Which hides your life and shows not half your parts." + 
+		"If I could write the beauty of your eyes " + 
+		"And in fresh numbers number all your graces," + 
+		"The age to come would say 'This poet lies:" + 
+		"Such heavenly touches ne'er touch'd earthly faces.'" + 
+		"So should my papers yellow'd with their age" + 
+		"Be scorn'd like old men of less truth than tongue, " + 
+		"And your true rights be term'd a poet's rage" + 
+		"And stretched metre of an antique song:" + 
+		"    But were some child of yours alive that time" + 
+		"    You should live twice; in it and in my rhyme.";
 		// 1. Create a String variable and initialize it to whatever you want
 
 		// 2. Print the 3rd char of your String to the console.
+String whatletter= "'";
 		//    HINT: .charAt
-		System.out.println(yeetyeetyeetyeet.charAt(3));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
 System.out.println(yeetyeetyeetyeet.length());
 for(int i=0;i<yeetyeetyeetyeet.length(); i++) {
-	if((yeetyeetyeetyeet.charAt(i)+"").equals("t")) {
-		System.out.println("the letter t is in the position "+(i+1)+".");
+	if((yeetyeetyeetyeet.charAt(i)+"").equalsIgnoreCase(whatletter)) {
+		System.out.println("the letter "+whatletter+" is in the position "+(i)+".");
 	}
 }
 		// 4. Using a for loop, print one char at time to the console.
--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,16 +15,20 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
-		// Convert their answer to an int using Integer.parseInt()
-
+String nickelString = JOptionPane.showInputDialog(null, "How many nickels do you have? I must know how much money I will have by the time I steal all of your nickels... ･｡ﾟ[̲̅$̲̅(̲̅ ° ͜ ʖ ° )̲̅$̲̅]｡ﾟ.*");
+		// Convert their answer to an bell using Integer.parseInt()
+int nickelInt=Integer.parseInt(nickelString);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimeString = JOptionPane.showInputDialog(null, "How many dimes do you have? I must know how much money I will have by the time I steal all of your dimes... ･｡ﾟ[̲̅$̲̅(̲̅ ° ͜ ʖ ° )̲̅$̲̅]｡ﾟ.*");
+int dimeInt = Integer.parseInt(dimeString);
 		// Ask the user how many quarters they have, and convert their answer
-
+String quarterString = JOptionPane.showInputDialog(null, "How many quarters do you have? I must know how much money I will have by the time I steal all of your quarters... ･｡ﾟ[̲̅$̲̅(̲̅ ° ͜ ʖ ° )̲̅$̲̅]｡ﾟ.*");
+int quarterInt = Integer.parseInt(quarterString);
 		// Calculate how much money the user has and save it in a double variable 
 
 		// Tell the user how much money they have
+double totalBalance = nickelInt*0.05+dimeInt*0.10+quarterInt*0.25;
+System.out.println(totalBalance);
 
 	}
 }
